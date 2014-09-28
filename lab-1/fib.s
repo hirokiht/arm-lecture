1eca09803d6fe9e2563bba02a14dfbe357310991
John Takeuchi <hirokiht@gmail.com>
2014-09-28 21:44:29 +0800

    Restructured for better code readability

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -11,19 +11,18 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
+	cmp r0, #1
+	it le
+	bxle lr		@return r0 if r0 <= 1
+
 	push {r4, r5, lr}
+	mov r4, r0
 
-	subs r4 ,r0, #1
-	bgt .L3
-	pop {r4, r5, pc}		@EPILOG
-	bx lr				@ R0 <= 1, return R0
-.L3:
-	mov r0, r4	@ Recursive call to fibonacci with R4 (R0 - 1) as parameter
+	sub r0 ,r0, #1	@ Recursive call to fibonacci with (R0 - 1) as parameter
 	bl fibonacci
-
 	mov r5,r0	@store result in R5
 
-	sub r0,r4,#1	@ Recursive call to fibonacci with R4 (R0 - 1) - 1 as parameter
+	sub r0, r4, #2	@ Recursive call to fibonacci with (R0 - 2) as parameter
 	bl fibonacci
 
 	add r0,r5,r0	@ R0 = R5 + R0
